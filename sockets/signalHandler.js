1e8a27dde1a89db7c6e528a609a9188d02e3fed4
shubh6p <prasadshubhambsp06@gmail.com>
2026-05-28 10:19:18 +0530

    Cool

--- sockets/signalHandler.js
+++ sockets/signalHandler.js
@@ -3,31 +3,46 @@ const { ensureSameRoom } = require('./state');
 const registerSignalHandlers = (io, socket) => {
   // WEBRTC SIGNALING: OFFER
   socket.on('signal-offer', (data) => {
-    if (data && data.targetPeerId && ensureSameRoom(socket.id, data.targetPeerId)) {
-      socket.to(data.targetPeerId).emit('signal-offer', {
-        offer: data.offer,
-        senderPeerId: socket.id
-      });
+    if (data && data.targetPeerId) {
+      if (ensureSameRoom(socket.id, data.targetPeerId)) {
+        console.log(`[SIGNAL] Relaying OFFER from ${socket.id} to ${data.targetPeerId}`);
+        socket.to(data.targetPeerId).emit('signal-offer', {
+          offer: data.offer,
+          senderPeerId: socket.id
+        });
+      } else {
+        console.warn(`[SIGNAL BLOCKED] OFFER from ${socket.id} to ${data.targetPeerId} - Not in same room`);
+      }
     }
   });
 
   // WEBRTC SIGNALING: ANSWER
   socket.on('signal-answer', (data) => {
-    if (data && data.targetPeerId && ensureSameRoom(socket.id, data.targetPeerId)) {
-      socket.to(data.targetPeerId).emit('signal-answer', {
-        answer: data.answer,
-        senderPeerId: socket.id
-      });
+    if (data && data.targetPeerId) {
+      if (ensureSameRoom(socket.id, data.targetPeerId)) {
+        console.log(`[SIGNAL] Relaying ANSWER from ${socket.id} to ${data.targetPeerId}`);
+        socket.to(data.targetPeerId).emit('signal-answer', {
+          answer: data.answer,
+          senderPeerId: socket.id
+        });
+      } else {
+        console.warn(`[SIGNAL BLOCKED] ANSWER from ${socket.id} to ${data.targetPeerId} - Not in same room`);
+      }
     }
   });
 
   // WEBRTC SIGNALING: ICE CANDIDATE
   socket.on('signal-ice-candidate', (data) => {
-    if (data && data.targetPeerId && ensureSameRoom(socket.id, data.targetPeerId)) {
-      socket.to(data.targetPeerId).emit('signal-ice-candidate', {
-        candidate: data.candidate,
-        senderPeerId: socket.id
-      });
+    if (data && data.targetPeerId) {
+      if (ensureSameRoom(socket.id, data.targetPeerId)) {
+        // console.log(`[SIGNAL] Relaying ICE from ${socket.id} to ${data.targetPeerId}`); // Too noisy, commented out
+        socket.to(data.targetPeerId).emit('signal-ice-candidate', {
+          candidate: data.candidate,
+          senderPeerId: socket.id
+        });
+      } else {
+        console.warn(`[SIGNAL BLOCKED] ICE from ${socket.id} to ${data.targetPeerId} - Not in same room`);
+      }
     }
   });
 };
